5f85530d9d5a8326b73bde4ac4ee68da8756d502
Christopher Swinton <chris@theswintons.co.uk>
2018-05-08 21:35:38 +0100

    I have now implemented some of the animation from an example into this code. I now need to get everything working and once that is done I can add the other functions that we want into it

--- Test/JSON Test/js/index.js
+++ Test/JSON Test/js/index.js
@@ -2,8 +2,17 @@ var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 var w = canvas.width/2;
 var h = canvas.height/2;
-function init()
+
+
+var angle = 3 * Math.PI / 180;
+var cx = canvas.width/2;
+var cy = canvas.height/2;
+var radius = 40;
+
+function draw(x,y)
 {
+  ctx.clearRect(0, 0, w*2, h*2);
+
 var Orbit = JSON.parse( '{"radius": 40, "colour": "none"}' ); // simmple object
 ctx.beginPath();
 ctx.arc(w,h,Orbit.radius,0,2*Math.PI); // draw the Orbit
@@ -71,6 +80,7 @@ ctx.fillStyle = Sphere3['colour']; // what colour?
 ctx.fill();
 
 var Sphere4 = JSON.parse( '{"radius": 10, "colour": "#ff0033"}' ); // simmple object
+
 ctx.beginPath();
 ctx.arc(w-90,h,Sphere4.radius,0,2*Math.PI); // draw the Sphere
 ctx.fillStyle = Sphere4['colour']; // what colour?
@@ -106,5 +116,59 @@ ctx.arc(w-420,h,Sphere9.radius,0,2*Math.PI); // draw the Sphere
 ctx.fillStyle = Sphere9['colour']; // what colour?
 ctx.fill();
 
+
+
+   // ctx.save();
+    var Sphere = JSON.parse( '{"radius": 40, "colour": "red"}' ); 
+
+    ctx.beginPath();
+    ctx.arc(x,y,Sphere.radius,0,2*Math.PI); // draw the Sphere
+    ctx.fillStyle = Sphere['colour']; // what colour?
+    ctx.fill();
+    ctx.restore();
+
+
+
+};
+
+function keepDrawing() {
+
+
+    draw(newX, newY);
+
+
+    setTimeout(keepDrawing, 250)
 }
-init();
+
+window.requestAnimFrame = (function (callback) {
+    return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame || function (callback) {
+        window.setTimeout(callback, 1000 / 60);
+    };
+})();
+
+var fps = 60;
+
+function animate() {
+    setTimeout(function () {
+        requestAnimationFrame(animate);
+
+        // increase the angle of rotation
+        angle += 3 * Math.PI / 180;
+
+        // calculate the new ball.x / ball.y
+        var newX = cx + radius * Math.cos(angle);
+        var newY = cy + radius * Math.sin(angle);
+        // draw
+        draw(newX, newY);
+
+        // draw the centerpoint 
+        ctx.beginPath();
+        ctx.arc(cx, cy, radius, 0, Math.PI * 2, false);
+        ctx.closePath();
+        ctx.stroke();
+
+    }, 1000 / fps);
+}
+animate();
+
+//init();
